352819f41423cb8140efe617c412ae27df33b9bb
jingyuexing <1941755817@qq.com>
2019-04-05 23:38:51 +0800

    Add Chained call

--- statistics/Variance.ts
+++ statistics/Variance.ts
@@ -1,16 +1,14 @@
 import {Mean} from "./Mean";
-class Variance{
 	/**
 	 * 方差的正平方根称为该随机变量的标准差
 	 * 方差
 	 */
-	constructor([...value]:Array<number>){
-		let mean:any = Mean([...value]);
-		let total:number=0;
-		for(let i=0;i<=value.length;i++){
-			total+=(value[i]-mean)**2;
+function Variance([...value]:Array<number>){
+		var mean:any = Mean([...value]);
+		var total:number=0;
+		for(let i:number=0;i<value.length-1;i++){
+			total+=Math.pow((value[i]-mean),2);
 		}
 		return total/value.length;
 	}
-}
 export {Variance}
--- test/gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require("gulp");
-const uglify = require("gulp-uglify");
-gulp.task(function(){
-    gulp.src("../") /*引入的需要对其执行操作的文件*/
-        .pipe(gulp()); /*对其执行的操作*/
-});
