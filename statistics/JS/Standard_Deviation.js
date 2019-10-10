1b2f8767a564264dc93e7f9715d4e18860b84355
jingyuexing <1941755817@qq.com>
2019-10-10 15:17:09 +0800

    删除不必要文件

--- statistics/JS/Standard_Deviation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var StandardDeviation = /** @class */ (function () {
-    function StandardDeviation() {
-    }
-    return StandardDeviation;
-}());
-exports.StandardDeviation = StandardDeviation;
