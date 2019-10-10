7fc7863c2ca85deb4955fe4d373aad281e705a3b
jingyuexing <1941755817@qq.com>
2019-10-10 15:16:02 +0800

    修改一些东西

---
+++ statistics/JS/Bayes.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * 贝叶斯
+ * 后验概率 = (似然性*先验概率)/标准化常量
+ */
+var bayes = /** @class */ (function () {
+    function bayes() {
+    }
+    return bayes;
+}());
+exports.bayes = bayes;
---
+++ statistics/JS/factorial.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * [Factorial 阶乘]
+ * @param  {number} n [阶乘数]
+ * @return {number}   [阶乘值]
+ */
+function Factorial(n) {
+    var total = 1, k = n % 1;
+    for (var i = 1; i <= n; i++) {
+        total *= i;
+    }
+    return total;
+}
+exports.Factorial = Factorial;
