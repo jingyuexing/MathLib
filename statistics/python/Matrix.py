da1696766db781e7c83afc9fb602f1a9e5086644
jingyuexing <1941755817@qq.com>
2019-12-01 16:28:34 +0800

    格式化文档

--- statistics/python/Matrix.py
+++ statistics/python/Matrix.py
@@ -9,26 +9,27 @@ class  Matrix:
     col 列数
     data 矩阵数据
     """
-    def __init__(self, row,col,data=[]):
+
+    def __init__(self, row, col, data=[]):
         self.row = row
         self.col = col
         self.data = data
-        for x in range(0,self.row):
+        for x in range(0, self.row):
             self.data.append([])
         for m in self.data:
-            for n in range(0,self.col):
+            for n in range(0, self.col):
                 m.append(None)
 
     def indentity(self):
         '''初始化为单位矩阵
-        
+
         [description]
-        
+
         Returns:
             [type] -- [description]
         '''
-        for i in range(0,self.col):
-            for j in range(0,self.row):
+        for i in range(0, self.col):
+            for j in range(0, self.row):
                 if i == j:
                     self.data[i][j] = 1
                 else:
@@ -62,100 +63,102 @@ class  Matrix:
             
     def hardamard(self,data):
         '''[summary]
-        
+
         [description]
-        
+
         Arguments:
             data {[type]} -- [description]
-        
+
         Returns:
             | -- [description]
         '''
-        if isinstance(data,Matrix):
-            tempMatrix = Matrix(data.row,data.col)
-            for i in range(0,data.col):
+        if isinstance(data, Matrix):
+            tempMatrix = Matrix(data.row, data.col)
+            for i in range(0, data.col):
                 for j in range(data.row):
-                    tempMatrix.data[i][j] = self.data[i][j]*data.data[i][j]
+                    tempMatrix.data[i][j] = self.data[i][j] * data.data[i][j]
             return tempMatrix
-        if isinstance(data,Vector):
+        if isinstance(data, Vector):
             tempVector = []
-            if len(data.data)==len(self.data):
-                for i in range(0,len(data.data)):
-                    for j in range(0,len(self.row)):
-                        tempVector = data.data[i]*self.data[i][j]
+            if len(data.data) == len(self.data):
+                for i in range(0, len(data.data)):
+                    for j in range(0, len(self.row)):
+                        tempVector = data.data[i] * self.data[i][j]
             return tempVector
 
-    def tr(self,Ma):
+    def tr(self, Ma):
         '''[summary]
-        
+
         矩阵迹运算
-        
+
         Arguments:
             Ma {Matrix} -- 需要迹运算的矩阵
-        
+
         Returns:
             number  -- 迹运算结果值
         '''
-        if isinstance(Ma,Matrix):
+        if isinstance(Ma, Matrix):
             total = 0
-            for i in range(0,Ma.col):
-                for j in range(0,Ma.row):
+            for i in range(0, Ma.col):
+                for j in range(0, Ma.row):
                     if i == j:
-                        total = total+Ma.data[i][j]
+                        total = total + Ma.data[i][j]
             return total
 
-    def frobenius(self,A):
+    def frobenius(self, A):
         '''[summary]
-        
+
         矩阵范数
-        
+
         Arguments:
             A {Matrix} -- 需要求范数的矩阵
-        
+
         Returns:
             number -- 范数值
         '''
         tempNums = 0
-        if isinstance(A,Matrix):
-            for i in range(0,A.row):
-                for j in range(0,A.col):
-                    tempNums = tempNums+ A.data[i][j]**2
+        if isinstance(A, Matrix):
+            for i in range(0, A.row):
+                for j in range(0, A.col):
+                    tempNums = tempNums + A.data[i][j]**2
             return tempNums**0.5
-    
-    def tran(self,A):
+
+    def tran(self, A):
         """矩阵转置
             A 需要被转置的矩阵
         """
-        if isinstance(A,Matrix):
-            NMatrix = Matrix(A.col,A.row)
-            for i in range(0,A.row):
-                for j in range(0,A.col):
+        if isinstance(A, Matrix):
+            NMatrix = Matrix(A.col, A.row)
+            for i in range(0, A.row):
+                for j in range(0, A.col):
                     NMatrix.data[j][i] = A.data[i][j]
             return NMatrix
+
     def map(self):
         '''[summary]
-        
+
         [description]
-        
+
         Returns:
             list -- 返回数组
         '''
-        tempArray=[]
-        for x in range(0,self.row):
-            for i in range(0,self.col):
+        tempArray = []
+        for x in range(0, self.row):
+            for i in range(0, self.col):
                 tempArray.append(self.data[x][i])
         return tempArray
-    def insertData(self,*args):
+
+    def insertData(self, *args):
         '''[summary]
-        
+
         为矩阵插入数据
-        
+
         Arguments:
             *args {Number} -- 数据
         '''
-        n=0
-        for i in range(0,self.row):
-            for j in range(0,self.col):
+        n = 0
+        for i in range(0, self.row):
+            for j in range(0, self.col):
                 if n >= len(args):
                     break
                 self.data[i][j] = args[n]
--- statistics/python/Vector.py
+++ statistics/python/Vector.py
@@ -2,137 +2,140 @@
 # @Author: Jingyuexing
 # @Date:   2019-07-12 00:24:07
 # @Last Modified by:   Admin
-# @Last Modified time: 2019-10-10 15:02:58
+# @Last Modified time: 2019-12-01 16:27:49
+
+
 class Vector:
     """docstring for Vector
     向量
     """
-    def __init__(self,data=[]):
-        if isinstance(data,list):
+
+    def __init__(self, data=[]):
+        if isinstance(data, list):
             self.data = data
         else:
             print("Error:data type is not list")
 
-
-    def add(self,data):
+    def add(self, data):
         '''向量相加
-        
+
         [description]
-        
+
         Arguments:
             data {Vector} -- 相加的向量
-        
+
         Returns:
             list -- 向量和值
         '''
-        tempAry=[]
-        if isinstance(data,Vector) and len(data.data)==len(self.data):
-            for i in range(0,len(data.data)):
-                tempAry.append(self.data[i]+data.data[i])
+        tempAry = []
+        if isinstance(data, Vector) and len(data.data) == len(self.data):
+            for i in range(0, len(data.data)):
+                tempAry.append(self.data[i] + data.data[i])
         return tempAry
 
-
-    def mult(self,data):
+    def mult(self, data):
         """[对向量两个向量进行求差]
-        
+
         [description]
-        
+
         Arguments:
             data {[Vector]} -- [相减向量]
-        
+
         Returns:
             [list] -- [向量和结果值]
         """
         tempAry = []
-        if isinstance(data,Vector) and len(data.data) == len(self.data):
-            for i in range(0,len(data.data)):
-                tempAry.append(data.data[i]-self.data[i])
+        if isinstance(data, Vector) and len(data.data) == len(self.data):
+            for i in range(0, len(data.data)):
+                tempAry.append(data.data[i] - self.data[i])
             return tempAry
         else:
             print("Error:参数必须是向量实例或两个向量长度需相同")
 
-    def mod(self,data):
+    def mod(self, data):
         '''向量求模
-        
+
         [description]
-        
+
         Arguments:
             data {Vecotr} -- 向量
-        
+
         Returns:
             number -- 模值
         '''
         total = 0
-        if isinstance(data,Vector):
-            for i in range(0,len(data.data)):
-                total = total+data.data[i]**2
-            return total**(1/2)
+        if isinstance(data, Vector):
+            for i in range(0, len(data.data)):
+                total = total + data.data[i]**2
+            return total**(1 / 2)
         else:
-            return TypeError(data+"is not Vector")
+            return TypeError(data + "is not Vector")
 
-    def product(self,data):
+    def product(self, data):
         '''向量乘积
-        
+
         >>>Vector_1 = Vector([2,3,4,5,6,7])
         >>>Vector_2 = Vector([12,5,78,2,12])
         >>>Vector_2.product(Vector_1) return 748:number
         >>>Vector_2.product(8)  return [96, 40, 624, 16, 96, 360]
         Arguments:
             data {number | list | Vector} -- 求点积或者向量乘积
-        
+
         Returns:
             number|list -- 向量乘积值
         '''
         total = 0
-        if isinstance(data,int) or isinstance(data,float):
-            for i in range(0,len(self.data)):
-                self.data[i]=self.data[i]*data
-        if isinstance(data,list):
+        if isinstance(data, int) or isinstance(data, float):
+            for i in range(0, len(self.data)):
+                self.data[i] = self.data[i] * data
+        if isinstance(data, list):
             if len(self.data) == data.__len__():
-                for i in range(0,len(data)):
-                    total = total + self.data[i]*data[i]
+                for i in range(0, len(data)):
+                    total = total + self.data[i] * data[i]
                 return total
             else:
                 return TypeError("参数长度不一致!")
-        if isinstance(data,Vector):
+        if isinstance(data, Vector):
             return self.product(data.data)
 
-    def isVertical(self,data):
+    def isVertical(self, data):
         '''判断是否和另外的向量垂直,垂直返回真,不垂直返回假
-        
-            
-        
+
+
+
         Arguments:
             data { Vector|List<number> } --  和自身比较的向量
-        
+
         Returns:
             bool --   是否垂直
         '''
-        if isinstance(data,list) and self.product(data) == 0:
+        if isinstance(data, list) and self.product(data) == 0:
             return True
-        if isinstance(data,Vector):
+        if isinstance(data, Vector):
             self.isVertical(data.data)
         return False
 
     def map(self):
         return self.data
-    def angle(self,data):
+
+    def angle(self, data):
         '''向量夹角
         Arguments:
             data {  Vector | list<number>} --   向量或者向量数据
-        
+
         Returns:
             number -- 结果值
         '''
-        if isinstance(data,list):
+        if isinstance(data, list):
             total = 0
-            if len(data)==len(self.data):
-                for i in range(0,len(data)):
-                    total = total+ self.data[i]*data[i]
-            return total/self.mod(data)*self.mod(self.data)
-        if isinstance(data,Vector):
+            if len(data) == len(self.data):
+                for i in range(0, len(data)):
+                    total = total + self.data[i] * data[i]
+            return total / self.mod(data) * self.mod(self.data)
+        if isinstance(data, Vector):
             self.angle(data.data)
-    def insertData(self,*args):
+
+    def insertData(self, *args):
         for i in args:
             self.data.append(i)
         return self
