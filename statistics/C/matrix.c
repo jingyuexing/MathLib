85e986f7dccdd3b81b208c6d27d0a02f3dd7d74b
jingyuexing <1941755817@qq.com>
2020-04-26 17:06:52 +0800

    add Haxe project

--- statistics/C/matrix.c
+++ statistics/C/matrix.c
@@ -1,6 +1,25 @@
 #include "matrix.h"
-int main(int argc, char const *argv[])
-{
-    Matrix a;
-    return 0;
+#include <stdio.h>
+Matrix Indentity(){
+    
+}
+Matrix Product(Matrix matrix){
+    
+}
+Matrix Hardamard(Matrix data){
+    Matrix matrix;
+}
+Matrix Tr(Matrix matrix){
+
+}
+Matrix DownDimensionality(Matrix matrix){
+}
+double* Frobenius(){
+
+}
+Matrix Tran(Matrix matrix){
+
+}
+double* Flat(Matrix Matrix){
+
 }
--- statistics/Haxe/src/core/Mean.hx
+++
@@ -1,9 +0,0 @@
-package core;
-class Mean {
-    public function new(data:Array<Float>):Void {
-        if(data!=null){
-            for(x in 1...data.length){
-            }
-        }
-    }
-}
