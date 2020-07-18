0deb762a93b126820799a283f27ce8ac7b7ae853
jingyuexing <1941755817@qq.com>
2020-07-19 05:39:48 +0800

    * 增加模块

--- statistics/python/Mean.py
+++ statistics/python/Mean.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 # @Author: Admin
 # @Date:   2020-01-10 00:57:06
-# @Last Modified by:   Jingyuexing
-# @Last Modified time: 2020-01-11 17:41:06
+# @Last Modified by:   jingyuexing
+# @Last Modified time: 2020-05-30 04:11:27
 
 from Rnak import Rank
+
+
 def mean(data=[]):
+    '''平均值
+
+    [description]
+
+    Keyword Arguments:
+        data {list} -- [description] (default: {[]})
+
+    Returns:
+        float -- 返回的平均值
+    '''
     total = 0
-    for i in range(0,len(data)):
-        total = total+data[i]
-    return total/len(data)
+    for i in range(0, len(data)):
+        total = total + data[i]
+    return total / len(data)
