308d749802d8e84a0a4ab7f02568a3fbca38590b
jingyuexing <1941755817@qq.com>
2020-07-19 06:59:37 +0800

    * format code

--- statistics/python/sigmoid.py
+++ statistics/python/sigmoid.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Author: Admin
 # @Date:   2020-01-10 02:04:02
-# @Last Modified by:   Admin
-# @Last Modified time: 2020-01-10 02:05:26
+# @Last Modified by:   jingyuexing
+# @Last Modified time: 2020-07-19 05:51:20
 
 def sigmoid(value):
     E = 2.718281828459045
     return 1/(1+E** -value)
-
-if __name__ == "__main__":
-    print(sigmoid(10))
-    print(sigmoid(14))
