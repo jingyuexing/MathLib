28138dff04e4b96bf5dbca1e661aaf6821b83a08
jingyuexing <1941755817@qq.com>
2020-07-19 06:59:37 +0800

    * format code

--- statistics/python/Summation.py
+++ statistics/python/Summation.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 # @Author: Admin
 # @Date:   2020-01-10 00:58:26
-# @Last Modified by:   Admin
-# @Last Modified time: 2020-01-10 01:30:19
+# @Last Modified by:   jingyuexing
+# @Last Modified time: 2020-07-19 05:44:01
+
+
 def summation(*args):
     total = 0
     tempList = []
     for x in args:
-        if isinstance(x,(tuple,list)):
+        if isinstance(x, (tuple, list)):
             for i in x:
                 tempList.append(i)
         else:
             tempList.append(x)
     for k in tempList:
-        total=total+k
+        total = total + k
     return total
-
-if __name__ == "__main__":
-    A = summation([12,5,6,7,8,9,0,12,44,6,77],[6,8,12,99,10])
-    print(A)
--- statistics/python/softmax.py
+++ statistics/python/softmax.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 # @Author: Admin
 # @Date:   2020-01-10 01:34:45
-# @Last Modified by:   Admin
-# @Last Modified time: 2020-01-10 02:12:26
+# @Last Modified by:   jingyuexing
+# @Last Modified time: 2020-07-19 05:41:21
+
+
 def exp(x=0):
     E = 2.718281828459045
     return E**x
+
 
 def softmax(*values):
     softmaxValue = []
     thisValues = []
     total = 0
     for k in values:
-        if isinstance(k,(list,tuple)):
+        if isinstance(k, (list, tuple)):
             for m in k:
                 thisValues.append(m)
     for i in thisValues:
-        total = total+exp(i)
+        total = total + exp(i)
     for n in thisValues:
-        softmaxValue.append(exp(n)/total)
+        softmaxValue.append(exp(n) / total)
     return softmaxValue
-if __name__ == '__main__':
-    v = softmax([1,2,3,4,5,6,7,8,9,10,11])
-    print(sum(v))
-    print(v)
