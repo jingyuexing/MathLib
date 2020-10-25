c7ed46ee1d0b308c9a9ae1247bfe516972f243c6
jingyuexing <MayorTalor@163.com>
2020-10-25 23:35:49 +0800

    更换项目目录

--- statistics/C/gcd.h
+++
@@ -1,5 +0,0 @@
-#ifndef _GCD_H
-#define _GCD_H
-//   定义最大公约数
-int gcd(int p,int q);
-#endif
--- statistics/C/sigmoid.c
+++
@@ -1,9 +0,0 @@
-#include "sigmoid.h"
-/**
- * [sigmoid description]
- * @param  x 输入数
- * @return   0-1之间的数
- */
-double sigmoid(double x){
-    return 1/(1+pow(E,-x));
-}
