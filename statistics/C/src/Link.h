d85c1c1b07f045a520aa12ad9b272e09841d9d51
jingyuexing <MayorTalor@163.com>
2020-12-08 15:47:33 +0800

    更改链表设计

--- statistics/C/src/Link.h
+++ statistics/C/src/Link.h
@@ -1,8 +1,14 @@
 #ifndef __LINK__
 #define __LINK__
+#include <stdlib.h>
 typedef struct Link{
     struct Link *child;
     struct Link *parent;
+    int index;
     double element;
 }Node;
+Node *find(Node *root,double item);
+Node *init(double element);
+Node *insert(Node *root,double item);
+Node last(Node *root);
 #endif
